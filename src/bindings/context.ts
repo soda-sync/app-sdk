024220fc5e603e3664be7cf52b399bf713f3cd51
Julian Finkler <julian@mintware.de>
2022-11-28 07:46:39 +0100

    App secrets added

--- src/bindings/context.ts
+++ src/bindings/context.ts
@@ -1,9 +1,10 @@
 import {KeyValueSet} from '../model/sdk/key-value-set';
+import {ReadonlyKeyValueSet} from '../model/sdk/readonly-key-value-set';
 
 export interface Context {
   /**
-   *  The current integration configuration.
-   *  DON'T USE THIS FOR STATE RELEVANT DATA.
+   * The current integration configuration.
+   * DON'T USE THIS FOR STATE RELEVANT DATA.
    */
   config: KeyValueSet;
 
@@ -12,4 +13,7 @@ export interface Context {
    * Use this to store state relevant data.
    */
   connectionConfig: KeyValueSet;
+
+  /** Configured app secrets. */
+  secrets: ReadonlyKeyValueSet;
 }
