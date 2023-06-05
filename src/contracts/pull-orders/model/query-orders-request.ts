b43feb0370f46662dddfc96ee1752ca836e320f8
Julian Finkler <julian@mintware.de>
2023-06-05 08:11:15 +0200

    change: Use generic request and result model

--- src/contracts/pull-orders/model/query-orders-request.ts
+++ src/contracts/pull-orders/model/query-orders-request.ts
@@ -1,29 +1,13 @@
 import {Filter, Join, NestedProperties} from '../../../sdk/types';
 import {OrderDto} from "../../orders/order-dto";
+import {QueryRequest} from "../../common/model/query-request";
 
 /**
  * A request object for querying orders.
  */
-export interface QueryOrdersRequest {
-  /**
-   * The token of the queried page.
-   * To request the first page, set it to null.
-   */
-  pageToken?: string;
-
-  /**
-   * The page size
-   * Must be >0
-   */
-  pageSize: number;
-
-  /**
-   * The filters to apply for query the orders
-   */
-  filters: Filter<OrderDto, Join<NestedProperties<OrderDto>, '.'>>[]
-
-  /**
-   * Custom data
-   */
-  attributes?: { [key: string]: any },
+export interface QueryOrdersRequest extends QueryRequest {
+    /**
+     * The filters to apply for query the orders
+     */
+    filters: Filter<OrderDto, Join<NestedProperties<OrderDto>, '.'>>[]
 }
--- src/contracts/pull-orders/model/query-orders-result.ts
+++ src/contracts/pull-orders/model/query-orders-result.ts
@@ -1,28 +1,7 @@
 import {OrderDto} from "../../orders/order-dto";
+import {QueryResult} from "../../common/model/query-result";
 
 /**
  * The query orders result
  */
-export interface QueryOrdersResult {
-    /**
-     * The token of the requested page.
-     */
-    pageToken?: string;
-
-    /**
-     * The page size
-     * Must be >0
-     */
-    pageSize: number;
-
-    /**
-     * The token of the next page.
-     * If there is no next page, leave it empty.
-     */
-    nextPageToken?: string;
-
-    /**
-     * The orders.
-     */
-    data: OrderDto[];
-}
+export type QueryOrdersResult = QueryResult<OrderDto>;
